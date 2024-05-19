bf78b77c746b35a55e1cb176924b8b6548143bd5
ImrichDiscantiny <55829428+ImrichDiscantiny@users.noreply.github.com>
2024-05-19 02:19:28 +0200

    removed slovak time zone

--- internal/stomatology_al/impl_stomatology_appointment_list.go
+++ internal/stomatology_al/impl_stomatology_appointment_list.go
@@ -15,32 +15,31 @@ func FormatDateString(y int, m time.Month, d int) string {
 	return fmt.Sprintf("%04d-%02d-%02d", y, int(m), d)
 }
 
+func isNotOldDate(appDate string) bool {
+	now := time.Now()
 
-func isNotOldDate(appDate string)(bool){
-    now := time.Now()
-	
 	y, m, d := now.Date()
 
-    dateStr := FormatDateString(y, m, d)
-	
-    CurrDate, err1 := time.Parse("2006-01-02", dateStr)
-    nextDate, err2 := time.Parse("2006-01-02", appDate)
-	
-    if err1 != nil {
-        // fmt.Println(err1)
+	dateStr := FormatDateString(y, m, d)
+
+	CurrDate, err1 := time.Parse("2006-01-02", dateStr)
+	nextDate, err2 := time.Parse("2006-01-02", appDate)
+
+	if err1 != nil {
+		// fmt.Println(err1)
 		return true
 	}
 
-    if err2 != nil {
-        // fmt.Println("error2")
+	if err2 != nil {
+		// fmt.Println("error2")
 		return true
 	}
 
-    if nextDate.Before(CurrDate) {
-        // fmt.Println(appDate, "is before", dateStr)
+	if nextDate.Before(CurrDate) {
+		// fmt.Println(appDate, "is before", dateStr)
 		return true
 	} else {
-        // fmt.Println(dateStr, "is before", appDate)
+		// fmt.Println(dateStr, "is before", appDate)
 		return false
 	}
 
@@ -50,225 +49,225 @@ func isNotOldDate(appDate string)(bool){
 
 // CreateAppointmentListEntry - Saves new entry into Appointment list
 func (this *implStomatologyAppointmentListAPI) CreateAppointmentListEntry(ctx *gin.Context) {
-  updateStomatologyFunc(ctx, func(c *gin.Context, stomatology *Stomatology) (*Stomatology,  interface{},  int){
-    var entry AppointmentListEntry
+	updateStomatologyFunc(ctx, func(c *gin.Context, stomatology *Stomatology) (*Stomatology, interface{}, int) {
+		var entry AppointmentListEntry
 
-    if err := c.ShouldBindJSON(&entry); err != nil {
-        return nil, gin.H{
-            "status": http.StatusBadRequest,
-            "message": "Invalid request body",
-            "error": err.Error(),
-        }, http.StatusBadRequest
-    }
+		if err := c.ShouldBindJSON(&entry); err != nil {
+			return nil, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Invalid request body",
+				"error":   err.Error(),
+			}, http.StatusBadRequest
+		}
 
-    if entry.Id == "" {
-        return nil, gin.H{
-            "status": http.StatusBadRequest,
-            "message": "Patient ID is required",
-        }, http.StatusBadRequest
-    }
+		if entry.Id == "" {
+			return nil, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Patient ID is required",
+			}, http.StatusBadRequest
+		}
 
-    isNotOld := isNotOldDate(entry.Date)
-    
-    if isNotOld {
-        return nil, gin.H{
-            "status": http.StatusBadRequest,
-            "message": "Cannot add old appointments",
-        }, http.StatusBadRequest
-    }
+		isNotOld := isNotOldDate(entry.Date)
 
-    if entry.Id == "" || entry.Id == "@new" {
-        entry.Id = uuid.NewString()
-    }
+		if isNotOld {
+			return nil, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Cannot add old appointments",
+			}, http.StatusBadRequest
+		}
 
-    conflictIndx := slices.IndexFunc( stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
-        return entry.Id == appointment.Id || (entry.Date == appointment.Date && entry.Duration == appointment.Duration) 
-    })
+		if entry.Id == "" || entry.Id == "@new" {
+			entry.Id = uuid.NewString()
+		}
 
-    if conflictIndx >= 0 {
-        return nil, gin.H{
-            "status": http.StatusConflict,
-            "message": "Entry already exists",
-        }, http.StatusConflict
-    }
+		conflictIndx := slices.IndexFunc(stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
+			return entry.Id == appointment.Id || (entry.Date == appointment.Date && entry.Duration == appointment.Duration)
+		})
 
-    stomatology.AppointmentList = append(stomatology.AppointmentList, entry)
-    // stomatology.reconcileAppointmentList()
+		if conflictIndx >= 0 {
+			return nil, gin.H{
+				"status":  http.StatusConflict,
+				"message": "Entry already exists",
+			}, http.StatusConflict
+		}
 
-    // entry was copied by value return reconciled value from the list
-    entryIndx := slices.IndexFunc( stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
-        return entry.Id == appointment.Id
-    })
-    if entryIndx < 0 {
-        return nil, gin.H{
-            "status": http.StatusInternalServerError,
-            "message": "Failed to save entry",
-        }, http.StatusInternalServerError
-    }
-    return stomatology, stomatology.AppointmentList[entryIndx], http.StatusOK
-})
+		stomatology.AppointmentList = append(stomatology.AppointmentList, entry)
+		// stomatology.reconcileAppointmentList()
+
+		// entry was copied by value return reconciled value from the list
+		entryIndx := slices.IndexFunc(stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
+			return entry.Id == appointment.Id
+		})
+		if entryIndx < 0 {
+			return nil, gin.H{
+				"status":  http.StatusInternalServerError,
+				"message": "Failed to save entry",
+			}, http.StatusInternalServerError
+		}
+		return stomatology, stomatology.AppointmentList[entryIndx], http.StatusOK
+	})
 }
 
 // DeleteAppointmentListEntry - Deletes specific entry
 func (this *implStomatologyAppointmentListAPI) DeleteAppointmentListEntry(ctx *gin.Context) {
-      updateStomatologyFunc(ctx, func(c *gin.Context, stomatology *Stomatology) (*Stomatology, interface{}, int) {
-        entryId := ctx.Param("id")
+	updateStomatologyFunc(ctx, func(c *gin.Context, stomatology *Stomatology) (*Stomatology, interface{}, int) {
+		entryId := ctx.Param("id")
 
-        if entryId == "" {
-            return nil, gin.H{
-                "status":  http.StatusBadRequest,
-                "message": "Entry ID is required",
-            }, http.StatusBadRequest
-        }
+		if entryId == "" {
+			return nil, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Entry ID is required",
+			}, http.StatusBadRequest
+		}
 
-        entryIndx := slices.IndexFunc(stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
-            return entryId == appointment.Id
-        })
+		entryIndx := slices.IndexFunc(stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
+			return entryId == appointment.Id
+		})
 
-        if entryIndx < 0 {
-            return nil, gin.H{
-                "status":  http.StatusNotFound,
-                "message": "Entry not found",
-            }, http.StatusNotFound
-        }
+		if entryIndx < 0 {
+			return nil, gin.H{
+				"status":  http.StatusNotFound,
+				"message": "Entry not found",
+			}, http.StatusNotFound
+		}
 
-        stomatology.AppointmentList = append(stomatology.AppointmentList[:entryIndx], stomatology.AppointmentList[entryIndx+1:]...)
-        // stomatology.reconcileWaitingList()
-        return stomatology, nil, http.StatusNoContent
-    })
+		stomatology.AppointmentList = append(stomatology.AppointmentList[:entryIndx], stomatology.AppointmentList[entryIndx+1:]...)
+		// stomatology.reconcileWaitingList()
+		return stomatology, nil, http.StatusNoContent
+	})
 }
 
 // GetAppointmentListEntries - Provides the ambulance Appointment list
 func (this *implStomatologyAppointmentListAPI) GetAppointmentListEntries(ctx *gin.Context) {
-  updateStomatologyFunc(ctx, func(c *gin.Context, stomatology *Stomatology) (*Stomatology, interface{}, int) {
-    
-    result := stomatology.AppointmentList
-    
-    if result == nil {
-        result = []AppointmentListEntry{}
-    }
-    
-    pivotDate := ctx.Param("appointmentsDate")
-    
-    date, err := time.Parse("2006-01-02", pivotDate)
-   
-    if err != nil {
-        return nil, gin.H{
-            "status":  http.StatusBadRequest,
-            "message": "Bad date",
-        }, http.StatusBadRequest
-    }
+	updateStomatologyFunc(ctx, func(c *gin.Context, stomatology *Stomatology) (*Stomatology, interface{}, int) {
 
-    var offsetStart int;
-    var offsetEnd int;
+		result := stomatology.AppointmentList
 
-    location, err := time.LoadLocation("Europe/Bratislava")
-    if err != nil {
-        return nil, gin.H{
-            "status":  http.StatusBadRequest,
-            "message": "Bad Slovak date",
-        }, http.StatusBadRequest
-    }
+		if result == nil {
+			result = []AppointmentListEntry{}
+		}
 
-    weekday := date.In(location).Weekday()
+		pivotDate := ctx.Param("appointmentsDate")
 
-    switch weekday {
-        case time.Monday:
-            offsetStart = 0
-            offsetEnd = 4
-        case time.Tuesday:
-            offsetStart = 1
-            offsetEnd = 3
-        case time.Wednesday:
-            offsetStart = 2
-            offsetEnd = 2
-        case time.Thursday:
-            offsetStart = 3
-            offsetEnd = 1
-        case time.Friday:
-            offsetStart = 4
-            offsetEnd = 0
-        case time.Saturday:
-            offsetStart = 5
-            offsetEnd = 0
-        case time.Sunday:
-            offsetStart = 6
-            offsetEnd = 0
-    }
-    
-    startDate := date.AddDate(0, 0, -offsetStart)
-    endDate := date.AddDate(0, 0, offsetEnd)
+		date, err := time.Parse("2006-01-02", pivotDate)
 
-    fmt.Println("start: ", startDate, "endDate: ", endDate)
-    var filtered []AppointmentListEntry
+		if err != nil {
+			return nil, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Bad date",
+			}, http.StatusBadRequest
+		}
 
-    for _, appointment := range result {
-        currDate, err := time.Parse("2006-01-02", appointment.Date) 
+		var offsetStart int
+		var offsetEnd int
 
-        if err == nil && (currDate.After(startDate) || currDate.Equal(startDate)) && 
-           (currDate.Before(endDate) || currDate.Equal(endDate)) {
-            filtered = append(filtered, appointment)
-        }
-    }
-    
-    return nil, filtered, http.StatusOK
-})
+		// location, err := time.LoadLocation("Europe/Bratislava")
+		// if err != nil {
+		//     return nil, gin.H{
+		//         "status":  http.StatusBadRequest,
+		//         "message": "Bad Slovak date",
+		//     }, http.StatusBadRequest
+		// }
+
+		weekday := date.Weekday()
+
+		switch weekday {
+		case time.Monday:
+			offsetStart = 0
+			offsetEnd = 4
+		case time.Tuesday:
+			offsetStart = 1
+			offsetEnd = 3
+		case time.Wednesday:
+			offsetStart = 2
+			offsetEnd = 2
+		case time.Thursday:
+			offsetStart = 3
+			offsetEnd = 1
+		case time.Friday:
+			offsetStart = 4
+			offsetEnd = 0
+		case time.Saturday:
+			offsetStart = 5
+			offsetEnd = 0
+		case time.Sunday:
+			offsetStart = 6
+			offsetEnd = 0
+		}
+
+		startDate := date.AddDate(0, 0, -offsetStart)
+		endDate := date.AddDate(0, 0, offsetEnd)
+
+		fmt.Println("start: ", startDate, "endDate: ", endDate)
+		var filtered []AppointmentListEntry
+
+		for _, appointment := range result {
+			currDate, err := time.Parse("2006-01-02", appointment.Date)
+
+			if err == nil && (currDate.After(startDate) || currDate.Equal(startDate)) &&
+				(currDate.Before(endDate) || currDate.Equal(endDate)) {
+				filtered = append(filtered, appointment)
+			}
+		}
+
+		return nil, filtered, http.StatusOK
+	})
 }
 
 // UpdateAppointmentListEntry - Updates specific entry
 func (this *implStomatologyAppointmentListAPI) UpdateAppointmentListEntry(ctx *gin.Context) {
-  updateStomatologyFunc(ctx, func(c *gin.Context, stomatology *Stomatology) (*Stomatology, interface{}, int) {
-    var entry AppointmentListEntry
+	updateStomatologyFunc(ctx, func(c *gin.Context, stomatology *Stomatology) (*Stomatology, interface{}, int) {
+		var entry AppointmentListEntry
 
-    if err := c.ShouldBindJSON(&entry); err != nil {
-        return nil, gin.H{
-            "status":  http.StatusBadRequest,
-            "message": "Invalid request body",
-            "error":   err.Error(),
-        }, http.StatusBadRequest
-    }
+		if err := c.ShouldBindJSON(&entry); err != nil {
+			return nil, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Invalid request body",
+				"error":   err.Error(),
+			}, http.StatusBadRequest
+		}
 
-    entryId := ctx.Param("id")
+		entryId := ctx.Param("id")
 
-    if entryId == "" {
-        return nil, gin.H{
-            "status":  http.StatusBadRequest,
-            "message": "Entry ID is required",
-        }, http.StatusBadRequest
-    }
+		if entryId == "" {
+			return nil, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Entry ID is required",
+			}, http.StatusBadRequest
+		}
 
-    isNotOld := isNotOldDate(entry.Date)
-    
-    if isNotOld {
-        return nil, gin.H{
-            "status": http.StatusBadRequest,
-            "message": "Cannot add old appointments",
-        }, http.StatusBadRequest
-    }
+		isNotOld := isNotOldDate(entry.Date)
 
-    entryIndxIn := slices.IndexFunc(stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
-        return entryId == appointment.Id
-    })
+		if isNotOld {
+			return nil, gin.H{
+				"status":  http.StatusBadRequest,
+				"message": "Cannot add old appointments",
+			}, http.StatusBadRequest
+		}
 
-    if entryIndxIn < 0 {
-        return nil, gin.H{
-            "status":  http.StatusNotFound,
-            "message": "Entry not found",
-        }, http.StatusNotFound
-    }
+		entryIndxIn := slices.IndexFunc(stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
+			return entryId == appointment.Id
+		})
 
-    DateMatchIndx := slices.IndexFunc(stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
-        return entryId != appointment.Id && entry.Date == appointment.Date && entry.Duration == appointment.Duration
-    })
+		if entryIndxIn < 0 {
+			return nil, gin.H{
+				"status":  http.StatusNotFound,
+				"message": "Entry not found",
+			}, http.StatusNotFound
+		}
 
-    if DateMatchIndx >= 0 {
-        return nil, gin.H{
-            "status":  http.StatusNotFound,
-            "message": "Time of Entry already exists, pick another one",
-        }, http.StatusNotFound
-    }
+		DateMatchIndx := slices.IndexFunc(stomatology.AppointmentList, func(appointment AppointmentListEntry) bool {
+			return entryId != appointment.Id && entry.Date == appointment.Date && entry.Duration == appointment.Duration
+		})
 
-    stomatology.AppointmentList[entryIndxIn] = entry
-    return stomatology, stomatology.AppointmentList[entryIndxIn], http.StatusOK
-})
+		if DateMatchIndx >= 0 {
+			return nil, gin.H{
+				"status":  http.StatusNotFound,
+				"message": "Time of Entry already exists, pick another one",
+			}, http.StatusNotFound
+		}
+
+		stomatology.AppointmentList[entryIndxIn] = entry
+		return stomatology, stomatology.AppointmentList[entryIndxIn], http.StatusOK
+	})
 }
